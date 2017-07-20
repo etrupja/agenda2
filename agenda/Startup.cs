f7963b1a2d2fb8e4ee075dbbb33c6b2e61664e78
GERMAN <GERMAN@DESKTOP-85RKQAL>
2017-07-19 20:03:21 -0700

    Add project files.

---
+++ agenda/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(agenda.Startup))]
+namespace agenda
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
